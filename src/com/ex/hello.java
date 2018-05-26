b64c65e79c4ee1042664e12d0c9b46d3b927a332
locul <locul@naver.com>
2018-05-26 15:15:22 +0900

    commit

--- src/com/ex/hello.java
+++ src/com/ex/hello.java
@@ -3,5 +3,8 @@ package com.ex;
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		
 	}
 }
